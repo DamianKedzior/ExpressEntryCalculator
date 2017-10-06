56e8a0b065d4881b3c1205068e74d19617ccb1f1
AnnaKed <annakedzior01@gmail.com>
2017-10-05 22:40:47 -0700

    CBL calculator for IELTS

--- (CRS) Criteria – Express Entry/LanguagePoints.cs
+++ (CRS) Criteria – Express Entry/LanguagePoints.cs
@@ -125,7 +125,166 @@ namespace _CRS__Criteria___Express_Entry
          
         public void CalculateCLBPoints()
         {
-            // zaimplementowac przeliczanie punktow na CLB punkty
+            
+            switch (LanguageExamType)
+            {
+                case LanguageExamTypes.IELTS:
+                    CalculateCLBPointsForIELTS();
+                    break;
+                case LanguageExamTypes.CELPIP:
+                    CalculateCLBPointsForCELPIP();
+                    break;
+                case LanguageExamTypes.TEF:
+                    CalculateCLBPointsForTEF();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+        private void CalculateCLBPointsForIELTS()
+        {
+            if (SpeakingPoints >= 7.5)
+            {
+                clbspeakingPoints = 10;
+            }
+            else if (SpeakingPoints == 7.0)
+            {
+                clbreadingPoints = 9;
+            }
+            else if (SpeakingPoints == 6.5)
+            {
+                clbreadingPoints = 8;
+            }
+            else if (SpeakingPoints == 6)
+            {
+                clbreadingPoints = 7;
+            }
+            else if (SpeakingPoints == 5.5)
+            {
+                clbreadingPoints = 6;
+            }
+            else if (SpeakingPoints == 5)
+            {
+                clbreadingPoints = 5;
+            }
+            else if (SpeakingPoints < 5 && SpeakingPoints>=4)
+            {
+                clbreadingPoints = 4;
+            }
+            else
+            {
+                clbreadingPoints = 0;
+            }
+
+            if (WritingPoints >= 7.5)
+            {
+                clbwritingPoints = 10;
+            }
+            else if (WritingPoints == 7)
+            {
+                clbwritingPoints = 9;
+            }
+            else if (WritingPoints == 6.5)
+            {
+                clbwritingPoints = 8;
+            }
+            else if (WritingPoints == 6)
+            {
+                clbwritingPoints = 7;
+            }
+            else if (WritingPoints == 5.5)
+            {
+                clbwritingPoints = 6;
+            }
+            else if (WritingPoints == 5)
+            {
+                clbwritingPoints = 5;
+            }
+            else if (WritingPoints < 5 && WritingPoints >= 4)
+            {
+                clbwritingPoints = 4;
+            }
+            else
+            {
+                clbwritingPoints = 0;
+            }
+
+            if (ReadingPoints >= 8)
+            {
+                clbreadingPoints = 10;
+            }
+            else if (ReadingPoints >= 7 && ReadingPoints < 8)
+            {
+                clbreadingPoints = 9;
+            }
+            else if (ReadingPoints == 6.5)
+            {
+                clbreadingPoints = 8;
+            }
+            else if (ReadingPoints == 6)
+            {
+                clbreadingPoints = 7;
+            }
+            else if (ReadingPoints >= 5 && ReadingPoints < 6)
+            {
+                clbreadingPoints = 6;
+            }
+            else if (ReadingPoints >= 4 && ReadingPoints < 5)
+            {
+                clbreadingPoints = 5;
+            }
+            else if (ReadingPoints == 3.5)
+            {
+                clbreadingPoints = 4;
+            }
+            else
+            {
+                clbreadingPoints = 0;
+            }
+
+            if (ListeningPoints >= 8.5)
+            {
+                clblisteningPoints = 10;
+            }
+            else if (ListeningPoints == 8)
+            {
+                clblisteningPoints = 9;
+            }
+            else if (ListeningPoints == 7.5)
+            {
+                clblisteningPoints = 8;
+            }
+            else if (ListeningPoints >= 6 && ListeningPoints < 7.5)
+            {
+                clblisteningPoints = 7;
+            }
+            else if (ListeningPoints == 5.5)
+            {
+                clblisteningPoints = 6;
+            }
+            else if (ListeningPoints == 5)
+            {
+                clblisteningPoints = 5;
+            }
+            else if (ListeningPoints == 4.5)
+            {
+                clblisteningPoints = 4;
+            }
+            else
+            {
+                clblisteningPoints = 0;
+            }
+                
+        }
+        private void CalculateCLBPointsForCELPIP()
+        {
+
+        }
+        private void CalculateCLBPointsForTEF()
+        {
+
         }
     }
 }
