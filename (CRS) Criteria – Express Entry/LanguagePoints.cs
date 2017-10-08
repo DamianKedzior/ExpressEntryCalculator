1c05a37a71715502dd6e589f06328a9f595ee876
AnnaKed <annakedzior01@gmail.com>
2017-10-07 21:44:13 -0700

    Finish calculating CLB points based on exam points

--- (CRS) Criteria – Express Entry/LanguagePoints.cs
+++ (CRS) Criteria – Express Entry/LanguagePoints.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _CRS__Criteria___Express_Entry
+﻿namespace _CRS__Criteria___Express_Entry
 {
     class LanguagePoints
     {
@@ -21,10 +15,10 @@ namespace _CRS__Criteria___Express_Entry
         double readingPoints;
         double listeningPoints;
 
-        double clbspeakingPoints;
-        double clbwritingPoints;
-        double clbreadingPoints;
-        double clblisteningPoints;
+        int clbspeakingPoints;
+        int clbwritingPoints;
+        int clbreadingPoints;
+        int clblisteningPoints;
 
         public LanguageExamTypes LanguageExamType
         {
@@ -91,7 +85,7 @@ namespace _CRS__Criteria___Express_Entry
         }
 
 
-        public double CLBSpeakingPoints
+        public int CLBSpeakingPoints
         {
             get
             {
@@ -99,7 +93,7 @@ namespace _CRS__Criteria___Express_Entry
             }
         }
 
-        public double CLBWritingPoints
+        public int CLBWritingPoints
         {
             get
             {
@@ -107,7 +101,7 @@ namespace _CRS__Criteria___Express_Entry
             }
         }
 
-        public double CLBReadingPoints
+        public int CLBReadingPoints
         {
             get
             {
@@ -115,7 +109,7 @@ namespace _CRS__Criteria___Express_Entry
             }
         }
 
-        public double CLBListeningPoints
+        public int CLBListeningPoints
         {
             get
             {
@@ -142,75 +136,47 @@ namespace _CRS__Criteria___Express_Entry
             }
         }
 
+        private int CalculateIELTStoCLB(double pointsIELTS)
+        {
+            if (pointsIELTS >= 7.5)
+            {
+                return 10;
+            }
+            else if (pointsIELTS == 7.0)
+            {
+                return 9;
+            }
+            else if (pointsIELTS == 6.5)
+            {
+                return 8;
+            }
+            else if (pointsIELTS == 6)
+            {
+                return 7;
+            }
+            else if (pointsIELTS == 5.5)
+            {
+                return 6;
+            }
+            else if (pointsIELTS == 5)
+            {
+                return 5;
+            }
+            else if (pointsIELTS < 5 && pointsIELTS >= 4)
+            {
+                return 4;
+            }
+            else
+            {
+                return 0;
+            }
+        }
 
         private void CalculateCLBPointsForIELTS()
         {
-            if (SpeakingPoints >= 7.5)
-            {
-                clbspeakingPoints = 10;
-            }
-            else if (SpeakingPoints == 7.0)
-            {
-                clbspeakingPoints = 9;
-            }
-            else if (SpeakingPoints == 6.5)
-            {
-                clbspeakingPoints = 8;
-            }
-            else if (SpeakingPoints == 6)
-            {
-                clbspeakingPoints = 7;
-            }
-            else if (SpeakingPoints == 5.5)
-            {
-                clbspeakingPoints = 6;
-            }
-            else if (SpeakingPoints == 5)
-            {
-                clbspeakingPoints = 5;
-            }
-            else if (SpeakingPoints < 5 && SpeakingPoints>=4)
-            {
-                clbspeakingPoints = 4;
-            }
-            else
-            {
-                clbspeakingPoints = 0;
-            }
-
-            if (WritingPoints >= 7.5)
-            {
-                clbwritingPoints = 10;
-            }
-            else if (WritingPoints == 7)
-            {
-                clbwritingPoints = 9;
-            }
-            else if (WritingPoints == 6.5)
-            {
-                clbwritingPoints = 8;
-            }
-            else if (WritingPoints == 6)
-            {
-                clbwritingPoints = 7;
-            }
-            else if (WritingPoints == 5.5)
-            {
-                clbwritingPoints = 6;
-            }
-            else if (WritingPoints == 5)
-            {
-                clbwritingPoints = 5;
-            }
-            else if (WritingPoints < 5 && WritingPoints >= 4)
-            {
-                clbwritingPoints = 4;
-            }
-            else
-            {
-                clbwritingPoints = 0;
-            }
-
+            clbspeakingPoints = CalculateIELTStoCLB(SpeakingPoints);
+            clbwritingPoints = CalculateIELTStoCLB(WritingPoints);
+       
             if (ReadingPoints >= 8)
             {
                 clbreadingPoints = 10;
@@ -276,15 +242,161 @@ namespace _CRS__Criteria___Express_Entry
             {
                 clblisteningPoints = 0;
             }
-                
         }
+
+        private int CalculateCELPIPtoCLB (double pointsCELPIP)
+        {
+            if (pointsCELPIP >= 10)
+            {
+                return 10;
+            }
+            else if (pointsCELPIP == 9)
+            {
+                return 9;
+            }
+            else if (pointsCELPIP == 8)
+            {
+                return 8;
+            }
+            else if (pointsCELPIP == 7)
+            {
+                return 7;
+            }
+            else if (pointsCELPIP == 6)
+            {
+                return 6;
+            }
+            else if (pointsCELPIP == 5)
+            {
+                return 5;
+            }
+            else if (pointsCELPIP == 4)
+            {
+                return 4;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
         private void CalculateCLBPointsForCELPIP()
         {
-
+            clbspeakingPoints = CalculateCELPIPtoCLB(SpeakingPoints);
+            clbwritingPoints = CalculateCELPIPtoCLB(WritingPoints);
+            clbreadingPoints = CalculateCELPIPtoCLB(ReadingPoints);
+            clblisteningPoints = CalculateCELPIPtoCLB(ListeningPoints);
         }
+
+        private int CalculateTEFtoCLB(double pointsTEF)
+        {
+            if (pointsTEF >= 393 && pointsTEF <= 415)
+            {
+                return 10;
+            }
+            else if (pointsTEF >= 371 && pointsTEF <= 392)
+            {
+                return 9;
+            }
+            else if (pointsTEF >= 349 && pointsTEF <= 370)
+            {
+                return 8;
+            }
+            else if (pointsTEF >= 310 && pointsTEF <= 348)
+            {
+                return 7;
+            }
+            else if (pointsTEF >= 271 && pointsTEF <= 309)
+            {
+                return 6;
+            }
+            else if (pointsTEF >= 226 && pointsTEF <= 270)
+            {
+                return 5;
+            }
+            else if (pointsTEF >= 181 && pointsTEF <= 225)
+            {
+                return 4;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
         private void CalculateCLBPointsForTEF()
         {
+            clbspeakingPoints = CalculateTEFtoCLB(SpeakingPoints);
+            clbwritingPoints = CalculateTEFtoCLB(WritingPoints);
 
+            // calculate reading points
+            if (ReadingPoints >= 263 && ReadingPoints <= 277)
+            {
+                clbreadingPoints = 10;
+            }
+            else if (ReadingPoints >= 248 && ReadingPoints <= 262)
+            {
+                clbreadingPoints = 9;
+            }
+            else if (ReadingPoints >= 233 && ReadingPoints <= 247)
+            {
+                clbreadingPoints = 8;
+            }
+            else if (ReadingPoints >= 207 && ReadingPoints <= 232)
+            {
+                clbreadingPoints = 7;
+            }
+            else if (ReadingPoints >= 181 && ReadingPoints <= 206)
+            {
+                clbreadingPoints = 6;
+            }
+            else if (ReadingPoints >= 151 && ReadingPoints <= 180)
+            {
+                clbreadingPoints = 5;
+            }
+            else if (ReadingPoints >= 121 && ReadingPoints <= 150)
+            {
+                clbreadingPoints = 4;
+            }
+            else
+            {
+                clbreadingPoints = 0;
+            }
+
+            // calculate listening points
+            if (ListeningPoints >= 316 && ListeningPoints <= 333)
+            {
+                clblisteningPoints = 10;
+            }
+            else if (ListeningPoints >= 298 && ListeningPoints <= 315)
+            {
+                clblisteningPoints = 9;
+            }
+            else if (ListeningPoints >= 280 && ListeningPoints <= 297)
+            {
+                clblisteningPoints = 8;
+            }
+            else if (ListeningPoints >= 249 && ListeningPoints <= 279)
+            {
+                clblisteningPoints = 7;
+            }
+            else if (ListeningPoints >= 217 && ListeningPoints <= 248)
+            {
+                clblisteningPoints = 6;
+            }
+            else if (ListeningPoints >= 181 && ListeningPoints <= 216)
+            {
+                clblisteningPoints = 5;
+            }
+            else if (ListeningPoints >= 145 && ListeningPoints <= 180)
+            {
+                clblisteningPoints = 4;
+            }
+            else
+            {
+                clblisteningPoints = 0;
+            }
         }
+
     }
 }
