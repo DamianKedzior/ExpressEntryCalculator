1275e69d163bdee0ae1c96a0b2a36afa0ebf17e6
AnnaKed <annakedzior01@gmail.com>
2017-11-20 17:48:39 -0800

    Rename solution and project

--- (CRS) Criteria – Express Entry/Properties/AssemblyInfo.cs
+++ (CRS) Criteria – Express Entry/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("(CRS) Criteria – Express Entry")]
+[assembly: AssemblyTitle("ExpressEntryCalculator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("(CRS) Criteria – Express Entry")]
+[assembly: AssemblyProduct("ExpressEntryCalculator")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
