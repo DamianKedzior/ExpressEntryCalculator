4db5d463d789dc73d12dab929b4eb997c1af8b37
Damian Kędzior <kedzior.damian@gmail.com>
2019-12-03 22:15:56 -0800

    add new function to host endpoint which calculates score

---
+++ ExpressEntryCalculator.Api/Models/ApplicantDataViewModel.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExpressEntryCalculator.Api.Models
+{
+    public class ApplicantDataViewModel
+    {
+        [Required]
+        [DataType(DataType.Date)] // TODO:DK might not be needed
+        public DateTime? BirthDate { get; set; }
+        public bool SpouseExist { get; set; }
+
+        [Required]
+        public ushort EducationLevel { get; set; }
+
+        public int? TypeOfFirstExam { get; set; }
+
+        public double SpeakingPoints { get; set; }
+        public double WritingPoints { get; set; }
+        public double ReadingPoints { get; set; }
+        public double ListeningPoints { get; set; }
+
+        public bool SecondLanguage { get; set; }
+        public int TypeOfSecondExam { get; set; }
+
+        public double SpeakingPointsSecondLanguage { get; set; }
+        public double WritingPointsSecondLanguage { get; set; }
+        public double ReadingPointsSecondLanguage { get; set; }
+        public double ListeningPointsSecondLanguage { get; set; }
+
+        public int CanadianExperience { get; set; }
+
+        [Required]
+        public ushort SpouseEducationLevel { get; set; }
+
+        public int? TypeOfSpouseExam { get; set; }
+        public double SpouseSpeakingPoints { get; set; }
+        public double SpouseWritingPoints { get; set; }
+        public double SpouseReadingPoints { get; set; }
+        public double SpouseListeningPoints { get; set; }
+
+        public int SpouseCanadianExperience { get; set; }
+
+        public int ExperienceOutsideCanada { get; set; }
+
+        public bool CanadianFamilyMember { get; set; }
+        public int CanadianEducation { get; set; }
+        public int CanadianArrangedEmployment { get; set; }
+        public bool CanadianProvincialOrTerritorialNomination { get; set; }
+
+        public ApplicantDataViewModel()
+        {
+            CanadianEducation = -1;
+        }
+    }
+}
---
+++ ExpressEntryCalculator.Api/Models/PointsSummaryViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace ExpressEntryCalculator.Api.Models
+{
+    public class PointsSummaryViewModel
+    {
+        public int PointsForAge { get; set; }
+        public int PointsForEducation { get; set; }
+        public int PointsForFirstLanguage { get; set; }
+        public int PointsForSecondLanguage { get; set; }
+        public int PointsForCanadianExperience { get; set; }
+        public int PointsInSectionA { get; set; }
+
+        public int PointsForSpouseEducation { get; set; }
+        public int PointsForSpouseLanguageExam { get; set; }
+        public int PointsForSpouseCanadianExperience { get; set; }
+        public int PointsInSectionB { get; set; }
+
+        public int PointsInSectionC { get; set; }
+
+        public int PointsInSectionD { get; set; }
+
+        public int TotalPointsForExpressEntry { get; set; }
+        //public ExpressEntryStats LastExpressEntryStats { get; set; } // TODO:DK
+    }
+}
